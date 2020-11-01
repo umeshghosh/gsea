2c985ed211af232c3e4efe70034b02c692eb5639
umeshghosh <fraternitydisposal@gmail.com>
2020-11-01 16:28:05 +0100

    Update gsea.py

--- gsea.py
+++ gsea.py
@@ -1,34 +1,26 @@
 import pandas as pd
 
-# glycolysis STAD c/s, ES 0.3
 # gene set
-s='HALLMARK_GLYCOLYSIS'
-#s='HALLMARK_INFLAMMATORY_RESPONSE'
-g=pd.read_table('/media/c/c/purity/gsea/db/h.all.v5.1.symbols.gmt',index_col=0).loc[s][1:].values
+g=pd.read_table('h.all.v5.1.symbols.gmt',index_col=0).loc['HALLMARK_GLYCOLYSIS'][1:].values
 
 # gseapy sort high to low
-d=pd.read_table('STAD.rnk',names=['lfc']).sort_values('lfc',ascending=0).reset_index()
+d=pd.read_table('example.rnk',names=['lfc']).sort_values('lfc',ascending=0).reset_index()
 d.columns='gene lfc'.split()
 
 # overlap
 d['hit']=(d['gene']).isin(g)
 
-# data (index, columns: gene, lfc, hit), gene_set 
+# Enrichment Score, data (index, columns: gene, lfc, hit), gene_set 
 def ES(d,g):
-
 	# norm
 	hit= 1./ d[d.hit==1].lfc.abs().sum()
-
 	es= ( d.hit*d.lfc.abs()*hit - (1.-d.hit)/len(d[d.hit==0]) ).cumsum()
-
 	# es
 	if np.abs(es.max()) > np.abs(es.min()):
 		es1=es.max()
 	else:
 		es1=es.min()
-
 	return es1
-
 es= ES(d,g)
 
 # random es, 1k
@@ -37,15 +29,15 @@ esr=[]
 # overlap
 o=len(d[d.hit==1])
 
+# Permulation test
 for i in range(1000):
 	sam=np.random.choice(len(d),o)
-
 	d['hit']=(d.index).isin(sam)
-
 	esr.append(ES(d,g))
 
 esr=pd.DataFrame(esr)
 
+# Normalized ES
 if es>0:
 	nes = es/esr[esr>=0].mean()
 else:
